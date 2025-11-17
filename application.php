af42827cf7d1873aa99293eba4941e41c2713b2f
spacerival <chantal.le21003@gmail.com>
2025-11-17 15:40:21 -0800

    improved application page

--- application.php
+++ application.php
@@ -17,42 +17,48 @@
 
     <!--TO DO: Fix the formatting (1) + Stylize (2)-->
     <div class="format">
-      <h1>PetMatch Application</h1>
+      <h1>PetMatch Application</h1><br>
       <form>
         <label for="first-name">First Name</label>
-        <input type="text" id="first-name" name="first-name">
+        <input type="text" id="first-name" name="first-name"
+          placeholder="Type here...">
         <label for="last-name">Last Name</label>
-        <input type="text" id="last-name" name="last-name"><br>
-        
+        <input type="text" id="last-name" name="last-name"
+          placeholder="Type here...">
+        <br><br>
 
         <label for="address">Address (Street Number + Name, City, Zipcode, Country) </label>
-        <input type="text" id="address" name="address" size="75"><br>
+        <input type="text" id="address" name="address" size="75"
+          placeholder="Type here..."><br>
+        <br>
 
         <label for="phone-number">Phone Number</label>
         <input type="tel" id="phone-number" name="phone-number" 
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="10">
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="10"
+            placeholder="Type here..."><br><br>
         <label for="email">Email</label>
-        <input type="text" id="email" name="email">
+        <input type="text" id="email" name="email" placeholder="Type here...">
+        <br><br>
       
 
         <h2>Housing Information</h2>
 
-        <p><b>What type of home do you currently live in?</b></p>
+        <p class="question"><b>What type of home do you currently live in?</b></p>
         <input type="radio" id="house" name="home-type" value="house">
         <label for="house">House</label><br>
         <input type="radio" id="apartment" name="home-type" value="apartment">
         <label  for="apartment">Apartment</label><br>
         <input type="radio" id="rv-trailer" name="home-type" value="rv-trailer">
         <label for="rv-trailer">RV/Trailer</label>
+        
 
-
-        <p><b>Do you rent or own?</b></p>
+        <p class="question"><b>Do you rent or own?</b></p>
         <input type="radio" id="renter" name="is-rent" value="renter">
         <label for="renter">Yes, I do rent</label><br>
         <input type="radio" id="homeowner" name="is-rent" value="homeowner">
         <labelfor="homeowner">No, I own my home</label>
 
-        <p><b>If you're <em>renting</em>, do you have permission from your landlord to keep pets?</b></p>
+        <p class="question"><b>If you're <em>renting</em>, do you have permission from your landlord to keep pets?</b></p>
         <input type="radio" id="landlord-allowed" name="landlord-permission" value="landlord-allowed">
         <label for="landlord-allowed">Yes</label><br>
         <input type="radio" id="landlord-unallowed" name="landlord-permission" value="landlord-unallowed">
@@ -62,14 +68,15 @@
 
 
         <h2>Pet Experience</h2>
-        <p><b>Have you owned a pet before?</b></p>
+        <p class="question"><b>Have you owned a pet before?</b></p>
         <input type="radio" id="has-pet-experience" name="pet-experience" value="has-pet-experience">
         <label for="has-pet-experience">Yes, I have owned a pet before</label><br>
         <input type="radio" id="no-pet-experience" name="pet-experience" value="no-pet-experience">
         <label for="no-pet-experience">No, this is my first time</label>
       
 
-        <p><b>If you <em>have</em> owned a pet before, what kinds?</b></p>
+        <p class="question"><b>If you <em>have</em> owned a pet before, what kinds? 
+          <i>Please select all that apply.</i></b></p>
         <input type="checkbox" id="owned-dog" name="owned-pet-type" value="owned-dog">
         <label for="owned-dog">Dog</label><br>
         <input type="checkbox" id="owned-cat" name="owned-pet-type" value="owned-cat">
@@ -79,22 +86,24 @@
         <input type="checkbox" id="owned-reptile" name="owned-pet-type" value="owned-reptile">
         <label for="owned-reptile">Reptile</label><br>
         <input type="checkbox" id="owned-rodent" name="owned-pet-type" value="owned-rodent">
-        <label for="owned-rodent">Rodent</label>
+        <label for="owned-rodent">Rodent</label><br>
+        <input type="checkbox" id="owned-other" name="owned-pet-type" value="owned-other">
+        <label for="owned-other">Other</label>
 
-        <p><b>Do you currently have other pets?</b></p>
+        <p class="question"><b>Do you currently have other pets?</b></p>
         <input type="radio" id="has-pet-experience" name="pet-experience" value="has-pet-experience">
         <label for="has-pet-experience">Yes, I do</label><br>
         <input type="radio" id="no-pet-experience" name="pet-experience" value="no-pet-experience">
         <label for="no-pet-experience">No, I don't</label>
 
-        <p><b>If you <em>do</em> currently own pets, please describe them</b></p>
-        <textarea id="pet-description" name="pet-description" rows="5" cols="75"></textarea>
+        <p class="question"><b>If you <em>do</em> currently own pets, please describe them</b></p>
+        <textarea id="pet-description" name="pet-description" rows="7" cols="75"></textarea>
         
         <h2>Household Information</h2>
         <label for="household-num">How many people currently live in your household?</label>
-        <input type="number" id="household-num" name="household-num">
+        <input type="number" id="household-num" name="household-num" placeholder="Type here...">
 
-        <p><b>Do you have children?</b></p>
+        <p class="question"><b>Do you have children?</b></p>
         <input type="radio" id="has-children" name="children-status" value="has-children">
         <label for="has-children">Yes, I do</label><br>
         <input type="radio" id="no-children" name="children-status" value="no-children">
@@ -102,25 +111,26 @@
 
         <!--Placeholder. Perhaps later have one input field ask how many children and 'x' amount of fields will
         pop up depending on the inputed value asking for the ages of each-->
-        <p><b>If you <em>do</em> have children, please state how many and hold old each of them are</b></p>
-        <textarea id="children-description" name="children-description" rows="5" cols="75"></textarea>
+        <p class="question"><b>If you <em>do</em> have children, please state how many and hold old each of them are</b></p>
+        <textarea id="children-description" name="children-description" rows="7" cols="75"></textarea>
 
         <h2>Adoption Questions</h2>
-        <p><b>Which pet are you interested in adopting?</b></p>
+        <p class="question"><b>Which pet are you interested in adopting?</b></p>
         <input type="text" placeholder="*later implement dropdown here*" size="25"><br>
         <p><b>Why do you want to adopt this pet?</b></p>
-        <textarea id="adoption-reason" name="adoption-reason" rows="5" cols="75"></textarea>
+        <textarea id="adoption-reason" name="adoption-reason" rows="7" cols="75"></textarea>
         <br>
         <label for="hours-pet-alone">How many hours a day will the pet be alone?</label>
         <select name="hours-pet-alone" id="hours-pet-alone">
-          <option value="alone-02">0 - 2</option>
-          <option value="alone-35">3 - 5</option>
-          <option value="alone-68">6 - 8</option>
-          <option value="alone-9+">9+</option>
+          <option value="alone-02">0 - 2 hours</option>
+          <option value="alone-35">3 - 5 hours</option>
+          <option value="alone-68">6 - 8 hours</option>
+          <option value="alone-9+">9+ hours</option>
         </select><br>
-        <input type="submit" value="Submit Application">
+        <button type="submit">Submit Application</button>
       </form>
     </div>
+    
   </body>
 </html>
 
