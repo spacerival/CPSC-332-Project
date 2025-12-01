00dc42794dd4adb7a6d89ea8b89b60e387bbcb24
spacerival <chantal.le21003@gmail.com>
2025-11-30 23:18:48 -0800

    fixed navbar

--- application_success.php
+++ application_success.php
@@ -1,3 +1,12 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
+
+$is_logged_in = isset($_SESSION['user_id']);
+$user_name = $is_logged_in ? $_SESSION['name'] : '';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
